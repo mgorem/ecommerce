c2032d2873e7015efb2a1d43a06d9d7da1f4ead8
omg <oremgitonga22@gmail.com>
2023-02-26 22:06:51 +0300

    do single product page

---
+++ ui/src/pages/Product.jsx
@@ -0,0 +1,156 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import product1 from "../assets/product1.jpeg";
+import { Add, Remove } from "@material-ui/icons";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const Select = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+  cursor: pointer;
+`;
+const Option = styled.option`
+  cursor: pointer;
+`;
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border-radius: 5px;
+  border: 2px solid teal;
+  cursor: pointer;
+  background-color: #fff;
+  font-weight: 500;
+
+  &:hover {
+    background-color: teal;
+    color: #fff;
+    transition: 0.3s ease;
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <ImgContainer>
+          <Image src={product1} />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Men's Suit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+            atque amet laboriosam quod magni autem non inventore blanditiis
+            mollitia repellat quos ipsam ipsum ducimus quas! Facilis enim
+            debitis suscipit eum?
+          </Desc>
+          <Price> KSH 7,100</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <Select>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+              </Select>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
