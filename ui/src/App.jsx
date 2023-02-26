4ad4c68996b9cecd21cecc39afcf80f5b320d7ba
omg <oremgitonga22@gmail.com>
2023-02-26 22:47:58 +0300

    register, login, now onto cart section

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,9 +1,12 @@
 // import Home from "./pages/Home";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Product />;
+  return <Cart />;
 };
 
 export default App;
