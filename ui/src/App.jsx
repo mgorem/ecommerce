7218ac9c3ee170a85a0d90c84c652ee4c56f333c
omg <oremgitonga22@gmail.com>
2023-02-27 13:22:37 +0300

    add react-router-dom

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,12 +1,25 @@
 import Home from "./pages/Home";
-// import Product from "./pages/Product";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import ProductList from "./pages/ProductList";
-// import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- ui/src/data/data.js
+++ ui/src/data/data.js
@@ -71,21 +71,31 @@ export const categories = [
     id: 1,
     img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "SHIRT STYLE!",
+    cat: "men",
   },
   {
     id: 2,
     img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "LOUNGEWEAR LOVE",
+    cat: "women",
   },
   {
     id: 3,
     img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     title: "LIGHT JACKETS",
+    cat: "boys",
   },
   {
     id: 4,
     img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     title: "LIGHTER JACKETS",
+    cat: "girls",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "ELECTRONICS",
+    cat: "electronics",
   },
 ];
 
